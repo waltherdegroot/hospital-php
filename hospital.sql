f9e4cfe1784388788586f42bf85c67ad2b3659a6
Maarten Nouwen <mnouwen@davinci.nl>
2017-04-18 10:15:19 +0200

    Update hospital.sql

--- hospital.sql
+++ hospital.sql
@@ -1,51 +1,42 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
---
--- Machine: 127.0.0.1
--- Gegenereerd op: 25 mrt 2015 om 01:41
--- Serverversie: 5.6.17
--- PHP-versie: 5.5.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Databank: `hospital`
---
 CREATE DATABASE IF NOT EXISTS `hospital` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `hospital`;
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `species`;
+CREATE TABLE IF NOT EXISTS `species` (
+	`species_id` int(11) NOT NULL AUTO_INCREMENT,
+	`species_description` varchar(50) DEFAULT NULL,
+	PRIMARY KEY (`species_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
---
--- Tabelstructuur voor tabel `patient`
---
+INSERT INTO `species` (`species_id`, `species_description`) VALUES
+(1, 'Hond'),
+(2, 'Kat');
 
-DROP TABLE IF EXISTS `patient`;
-CREATE TABLE IF NOT EXISTS `patient` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  `species` varchar(50) DEFAULT NULL,
-  `status` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+DROP TABLE IF EXISTS `clients`;
+CREATE TABLE IF NOT EXISTS `clients` (
+	`client_id` int(11) NOT NULL AUTO_INCREMENT,
+	`client_firstname` varchar(50) DEFAULT NULL,
+	`client_lastname` varchar(50) DEFAULT NULL,
+	PRIMARY KEY (`client_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
---
--- Gegevens worden geëxporteerd voor tabel `patient`
---
+INSERT INTO `clients` (`client_id`, `client_firstname`, `client_lastname`) VALUES
+(1, 'Jane', 'Doe'),
+(2, 'John', 'Doe');
 
-INSERT INTO `patient` (`id`, `name`, `species`, `status`) VALUES
-(1, 'Bobbie', 'hond', 'Koorts, eet slecht, blaft veel te veel'),
-(2, 'Minoes', 'kat', 'Drinkt niet, haaruitval, mager'),
-(3, 'Kees', 'hond', 'Eet te veel, vetzucht, jankt en kotst');
+DROP TABLE IF EXISTS `patients`;
+CREATE TABLE IF NOT EXISTS `patients` (
+  `patient_id` int(11) NOT NULL AUTO_INCREMENT,
+  `patient_name` varchar(50) DEFAULT NULL,
+  `species_id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `patient_status` text,
+  PRIMARY KEY (`patient_id`),
+  FOREIGN KEY (`species_id`) REFERENCES `species`(`species_id`),
+  FOREIGN KEY (`client_id`) REFERENCES `clients`(`client_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `patients` (`patient_id`, `patient_name`, `species_id`, `client_id` ,`patient_status`) VALUES
+(1, 'Bobbie', 1, 1,'Koorts, eet slecht, blaft veel te veel'),
+(2, 'Minoes', 2, 2,'Drinkt niet, haaruitval, mager'),
+(3, 'Kees', 1, 2,'Eet te veel, vetzucht, jankt en kotst');
